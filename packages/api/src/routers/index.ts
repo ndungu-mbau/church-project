b8f1b493da30af157b94050d2f6199c1371b492c
Nelson Mbau <mbau.ndungu@gmail.com>
2025-11-30 21:00:32 +0300

    feat: add invites router

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,5 +1,6 @@
 import { protectedProcedure, publicProcedure, router } from "../index";
 import { todoRouter } from "./todo";
+import { invitesRouter } from "./invites";
 
 export const appRouter = router({
 	healthCheck: publicProcedure.query(() => {
@@ -12,5 +13,6 @@ export const appRouter = router({
 		};
 	}),
 	todo: todoRouter,
+	invites: invitesRouter,
 });
 export type AppRouter = typeof appRouter;
---
+++ packages/api/src/routers/invites.ts
@@ -0,0 +1,25 @@
+import z from "zod";
+import { router, publicProcedure, superuserProcedure } from "../index";
+import { invites } from "@church-project/db/schema/invites";
+import { eq } from "drizzle-orm";
+import { db } from "@church-project/db";
+
+export const invitesRouter = router({
+  getAll: superuserProcedure.query(async () => {
+    return await db.select().from(invites);
+  }),
+
+  create: publicProcedure
+    .input(z.object({ email: z.string().email() }))
+    .mutation(async ({ input }) => {
+      return await db.insert(invites).values({
+        email: input.email,
+      });
+    }),
+
+  delete: superuserProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ input }) => {
+      return await db.delete(invites).where(eq(invites.id, input.id));
+    }),
+});
