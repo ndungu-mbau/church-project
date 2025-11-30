49d64568e5c10a10e687ffa51f42da716afd8f7b
Nelson Mbau <mbau.ndungu@gmail.com>
2025-11-30 19:00:57 +0300

    feat: cors change to allow all origins to pass through

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: process.env.CORS_ORIGIN || "",
+		origin: true,
 		methods: ["GET", "POST", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 		credentials: true,
