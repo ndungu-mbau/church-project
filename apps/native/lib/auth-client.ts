40b750bd77622869e849b2b6654268aea3fd0fa2
Nelson Mbau <mbau.ndungu@gmail.com>
2025-11-30 19:02:28 +0300

    feat: configure auth, trps clients to use base url fn

--- apps/native/lib/auth-client.ts
+++ apps/native/lib/auth-client.ts
@@ -1,11 +1,15 @@
 import { expoClient } from "@better-auth/expo/client";
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "@church-project/auth";
 import * as SecureStore from "expo-secure-store";
 import Constants from "expo-constants";
+import { getBaseUrl } from "@/utils/base-url";
 
 export const authClient = createAuthClient({
-	baseURL: process.env.EXPO_PUBLIC_SERVER_URL,
+	baseURL: getBaseUrl(),
 	plugins: [
+		inferAdditionalFields<typeof auth>(),
 		expoClient({
 			scheme: Constants.expoConfig?.scheme as string,
 			storagePrefix: Constants.expoConfig?.scheme as string,
--- apps/native/utils/trpc.ts
+++ apps/native/utils/trpc.ts
@@ -3,13 +3,14 @@ import { QueryClient } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import type { AppRouter } from "@church-project/api/routers/index";
+import { getBaseUrl } from "./base-url";
 
 export const queryClient = new QueryClient();
 
 const trpcClient = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
-			url: `${process.env.EXPO_PUBLIC_SERVER_URL}/trpc`,
+			url: `${getBaseUrl()}/trpc`,
 			headers() {
 				const headers = new Map<string, string>();
 				const cookies = authClient.getCookie();
